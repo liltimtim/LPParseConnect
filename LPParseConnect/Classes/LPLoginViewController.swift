4b0294e616b766f6ace9629b37bf0b3cd7f7ca02
Timothy Dillman <tim@launchpeer.com>
2017-07-24 13:16:48 -0400

    added login view controller for presentation and simple login

---
+++ LPParseConnect/Classes/LPLoginViewController.swift
@@ -0,0 +1,75 @@
+//
+//  LoginViewController.swift
+//  Pods
+//
+//  Created by Timothy Dillman on 7/24/17.
+//
+//
+
+import UIKit
+import SwiftValidator
+public protocol LPLoginViewControllerProtocol: class {
+    func didFinishAuthenticating()
+}
+public class LPLoginViewController: UIViewController, LPAuthEmittableProtocol {
+    @IBOutlet public weak var usernameField:UITextField!
+    @IBOutlet public weak var passwordField:UITextField!
+    @IBOutlet public weak var loginBtn:UIButton_LPActivity!
+    public weak var delegate:LPLoginViewControllerProtocol?
+    internal var validator:Validator = Validator()
+    internal var provider:StandardAuthProvider?
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        validator.registerField(usernameField, rules: [RequiredRule(), EmailRule()])
+        validator.registerField(passwordField, rules: [RequiredRule()])
+        setupFields()
+        loginBtn.addTarget(self, action: #selector(loginPressed), for: .touchUpInside)
+    }
+
+    override public func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    internal func loginPressed() {
+        loginBtn.beginActivity()
+        validator.validate(self)
+    }
+    
+    internal func login(username:String, password:String) {
+        provider = StandardAuthProvider.init(delegate: self)
+        provider?.authenticate(username: username, password: password)
+    }
+    
+    public func didEmit(error: Error) {
+        loginBtn.endActivity()
+        self.present(UIAlertController.createErrorAlert(error: error), animated: true, completion: nil)
+    }
+    
+    public func didAuthenticate() {
+        loginBtn.endActivity()
+        self.delegate?.didFinishAuthenticating()
+    }
+    
+    public func setupFields() {
+        usernameField.autocorrectionType = .no
+        usernameField.autocapitalizationType = .none
+        passwordField.isSecureTextEntry = true
+    }
+}
+
+extension LPLoginViewController : ValidationDelegate {
+    public func validationSuccessful() {
+        self.login(username: usernameField.text!, password: passwordField.text!)
+    }
+    
+    public func validationFailed(_ errors: [(Validatable, ValidationError)]) {
+        loginBtn.endActivity()
+        for(item, _) in errors {
+            if let field = item as? UITextField {
+                field.invalidate()
+            }
+        }
+    }
+}
+
+
